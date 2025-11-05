0a13f9236335d82818b64b02f52f9e4f543d129a
Arun Sabu <sabuarun00@gmail.com>
2025-11-05 20:47:17 +0530

    Updated 04-string-search.js and added 05-string-reference.js

---
+++ JS Numbers/01-numbers.js
@@ -0,0 +1,13 @@
+// JavaScript Numbers
+
+/*
+Number Types
+JavaScript has only one type of number.
+
+Numbers can be written with or without decimals.
+*/
+let x = 3.14; // A number with decimals
+let y = 3; // A number without decimals
+console.log(x+ " " + y)
+
+// Extra large or extra small numbers can be written with scientific (exponent) notation:
